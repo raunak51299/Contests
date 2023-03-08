01c29d3a060178d8bce60e033d56f19ec7577edb
Raunak <raunak51299@gmail.com>
2023-03-08 18:57:37 +0530

    a2oj

---
+++ codeforces/practice/D_Same_Differences.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define us unordered_set<int>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long>
+#define vvl vector<vl>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vb vector<bool>
+#define ll long long
+#define pb push_back
+#define mem1(a) memset(a, -1, sizeof(a))
+#define mem0(a) memset(a, 0, sizeof(a))
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+#define mp make_pair
+#define rep(a, b, c) for (long long a = b; a <= c; ++a)
+#define repr(a, b, c) for (long long a = b; a >= c; --a)
+#define input(a, b, n)                 \
+    for (long long i = b; i <= n; ++i) \
+        cin >> a[i];
+#define output(a, b, n)                \
+    for (long long i = b; i <= n; ++i) \
+        cout << a[i] << " ";
+#define Mod 1000000007
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vll v(n);
+    input(v, 0, n - 1);
+    ll ans = 0;
+    unordered_map<ll, ll> mp;
+    for (int i = 0; i < n; i++)
+    {
+        mp[v[i] - i]++;
+    }
+    for (auto it : mp)
+    {
+        ans += (it.second * (it.second - 1)) / 2;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
